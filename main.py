7bae5615c77b97949f7b7432c928c403b244c3a2
bauti <jnemi@fie.undef.edu.ar>
2021-09-13 19:03:25 -0300

    ADD - Assignment a

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
 
 packages = open("envios.txt", "rt")
+threshold = 50000
 
-line = packages.readline()
+
+def log(file_name, text_line):
+    log_file = open(file_name, 'a')
+    log_file.write(text_line)
+    log_file.close()
+
+
+line = packages.readline().strip()
 while line != "":
     data = line.split("-")
-    print(data)
-    line = packages.readline()
+    if int(data[0]) < threshold:
+        log("menos.txt", "-".join(data) + "\n")
+    line = packages.readline().strip()
 
 packages.close()

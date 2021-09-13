0853f0e47d571793d728f34263fc1d487b9859f6
bauti <jnemi@fie.undef.edu.ar>
2021-09-13 19:14:18 -0300

    ADD - Assignment c

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
-packages = open("envios.txt", "rt")
+shipment = open("envios.txt", "rt")
 threshold = 50000
+vat = 1.21
 
 
 def log(file_name, text_line):
@@ -10,13 +11,22 @@ def log(file_name, text_line):
     log_file.close()
 
 
-line = packages.readline().strip()
+line = shipment.readline().strip()
 while line != "":
     data = line.split("-")
+
+    # log products with code under threshold
     if int(data[0]) < threshold:
         log("menos.txt", data[0] + "-" + data[1] + "\n")
+
+    # log products shipped internationally
     if data[3] != data[5]:
         log("internacionales.txt", "-".join(data) + "\n")
-    line = packages.readline().strip()
 
-packages.close()
+    # calculate VAT
+    final_price = float(data[6]) * vat
+    log("con_iva.txt", data[1] + "-" + data[7] + "-" + str(final_price) + "\n")
+
+    line = shipment.readline().strip()
+
+shipment.close()

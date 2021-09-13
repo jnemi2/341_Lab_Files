18bc563b4a9b5c92a443fa74f63d9e8e0d85765c
bauti <jnemi@fie.undef.edu.ar>
2021-09-13 19:25:27 -0300

    ADD - Assignment d

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime.datetime as date
+import random
 import os
 
 shipment = open("envios.txt", "rt")
@@ -26,6 +28,15 @@ while line != "":
     # calculate VAT
     final_price = float(data[6]) * vat
     log("con_iva.txt", data[1] + "-" + data[7] + "-" + str(final_price) + "\n")
+
+    # generate processed_file
+    line_processed = data[0] + "-" + data[1].upper() + "-" + str(date.now()) + "-" + \
+                     str(random.randint(1000000000, 9999999999))
+    if data[8] == "True":
+        line_processed = line_processed + "\n"
+    else:
+        line_processed = line_processed + "-" + data[6] + "\n"
+    log("procesados.txt", line_processed)
 
     line = shipment.readline().strip()
 
